a2356e7b15a0102418cbc37d96c669adae3ac423
Morten Bøhne <github@mortenbohne.dk>
2025-04-01 15:07:50 +0200

    renaming of dag-nodes

--- src/mb/nodes/node_types.py
+++ src/mb/nodes/node_types.py
@@ -1,6 +1,6 @@
 from mb.log import get_logger
 import mb.nodes.cast as cast
-from mb import OpenMaya2
+from mb import OpenMaya2, cmds
 
 logger = get_logger(__name__)
 logger.setLevel("DEBUG")
@@ -9,6 +9,16 @@ logger.setLevel("DEBUG")
 class Node:
     def __init__(self, node_path: str):
         self.node_path = node_path
+
+    def rename(self, new_name: str) -> str:
+        """
+        Rename the current node. If new name is not unique maya will resolve a new unique name.
+        :param new_name: The new name of the node.
+        :return: The new name of the node.
+        """
+        actual_new_name = cmds.rename(str(self), new_name)
+        self.node_path = actual_new_name
+        return actual_new_name
 
 
 class DagNode(Node):
--- tests/mb/nodes/test_node_types.py
+++ tests/mb/nodes/test_node_types.py
@@ -10,3 +10,18 @@ def test_dag_node_string(test_cube, caplog):
     mb.cmds.delete(test_cube)
     with caplog.at_level(logging.WARNING):
         node_path = str(dag_node)
+
+
+def test_dag_node_rename(test_cube, caplog):
+    dag_node = mb.nodes.node_types.DagNode(test_cube)
+    new_name = "new_name"
+    new_resolved_name = dag_node.rename(new_name)
+    assert new_resolved_name == new_name
+    another_cube = mb.cmds.polyCube(name="another_cube")[0]
+    another_dag_node = mb.nodes.node_types.DagNode(another_cube)
+    another_cube_new_name = another_dag_node.rename(new_name)
+    assert another_cube_new_name != new_name
+    assert another_cube_new_name == "new_name1"
+    another_dag_node_full_path = f"|{another_cube_new_name}"
+    assert str(another_dag_node) == another_dag_node_full_path
+    assert another_dag_node.dag_path.fullPathName() == another_dag_node_full_path
