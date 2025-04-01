5e7ab6ecaba5360214d593447d0ffcde912855fb
Morten Bøhne <github@mortenbohne.dk>
2025-04-01 15:25:34 +0200

    test that nodepath is updated when node is renamed, gives correct warning when accessing deleted node

--- tests/mb/nodes/test_node_types.py
+++ tests/mb/nodes/test_node_types.py
@@ -7,9 +7,14 @@ def test_dag_node_string(test_cube, caplog):
     dag_node = mb.nodes.node_types.DagNode(test_cube)
     full_node_path = mb.nodes.cast.get_long_name_from_maya_string(test_cube)
     assert str(dag_node) == full_node_path
-    mb.cmds.delete(test_cube)
+    # rename to see warning display correct node path when deleted
+    new_name = "new_name"
+    dag_node.rename(new_name)
+    mb.cmds.delete(str(dag_node))
     with caplog.at_level(logging.WARNING):
-        node_path = str(dag_node)
+        str(dag_node)
+        node_path = dag_node.node_path
+        assert node_path in str(caplog.text)
 
 
 def test_dag_node_rename(test_cube, caplog):
