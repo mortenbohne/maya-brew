d5bdf1d3c0853fa47d9b00b2814947f58a11c405
Morten Bøhne <github@mortenbohne.dk>
2025-04-01 11:51:23 +0200

    added basic node fixtures

--- tests/mb/conftest.py
+++ tests/mb/conftest.py
@@ -1,5 +1,22 @@
 import pytest
+import mb
 import mb.scene
+
+
+@pytest.fixture()
+def test_namespace():
+    return "my_namespace"
+
+
+@pytest.fixture()
+def test_cube_name():
+    return "test_cube"
+
+
+@pytest.fixture()
+def test_cube(test_namespace, test_cube_name):
+    cube, history = mb.cmds.polyCube(name=f"{test_namespace}:{test_cube_name}")
+    yield cube
 
 
 @pytest.fixture(autouse=True)
--- tests/mb/nodes/test_cast.py
+++ tests/mb/nodes/test_cast.py
@@ -3,21 +3,15 @@ import mb
 import mb.nodes.cast
 
 
-def test_string_to_dag_path():
-    namespace_name = "my_new_namespace"
-    cube, history = mb.cmds.polyCube(name=f"{namespace_name}:my_cube")
-    cube_full_path = mb.cmds.ls(cube, long=True)[0]
-    assert namespace_name in cube_full_path
+def test_string_to_dag_path(test_cube, test_namespace, test_cube_name):
+    cube_full_path = mb.cmds.ls(test_cube, long=True)[0]
+    assert cube_full_path == f"|{test_namespace}:{test_cube_name}"
     dag_path = mb.nodes.cast.get_dag_path_from_string(cube_full_path)
     assert isinstance(dag_path, mb.OpenMaya2.MDagPath)
 
 
-
-def test_invalid_string_to_dag_path():
-    namespace_name = "my_new_namespace"
-    cube_short_name = "my_cube"
-    mb.cmds.polyCube(name=f"{namespace_name}:{cube_short_name}")
+def test_invalid_string_to_dag_path(test_cube, test_cube_name):
     with pytest.raises(mb.nodes.cast.InvalidDagPath):
         mb.nodes.cast.get_dag_path_from_string("invalid_name")
     with pytest.raises(mb.nodes.cast.InvalidDagPath):
-        mb.nodes.cast.get_dag_path_from_string("cube_short_name")
+        mb.nodes.cast.get_dag_path_from_string(test_cube_name)
