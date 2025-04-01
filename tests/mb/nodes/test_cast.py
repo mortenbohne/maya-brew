33f3b92bf1acf4121548216d0cce9c56401bf5ea
Morten Bøhne <github@mortenbohne.dk>
2025-04-01 11:12:12 +0200

    cleanup of unused variables in tests

--- tests/mb/nodes/test_cast.py
+++ tests/mb/nodes/test_cast.py
@@ -16,8 +16,8 @@ def test_string_to_dag_path():
 def test_invalid_string_to_dag_path():
     namespace_name = "my_new_namespace"
     cube_short_name = "my_cube"
-    cube, history = mb.cmds.polyCube(name=f"{namespace_name}:{cube_short_name}")
+    mb.cmds.polyCube(name=f"{namespace_name}:{cube_short_name}")
     with pytest.raises(mb.nodes.cast.InvalidDagPath):
-        invalid_dag_path = mb.nodes.cast.get_dag_path_from_string("invalid_name")
+        mb.nodes.cast.get_dag_path_from_string("invalid_name")
     with pytest.raises(mb.nodes.cast.InvalidDagPath):
-        invalid_dag_path = mb.nodes.cast.get_dag_path_from_string("cube_short_name")
+        mb.nodes.cast.get_dag_path_from_string("cube_short_name")
