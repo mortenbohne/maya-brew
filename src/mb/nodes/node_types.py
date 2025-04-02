6dfbb2c9aeb7af87266c7c11bfebf894ed91348d
Morten Bøhne <github@mortenbohne.dk>
2025-04-02 10:01:38 +0200

    adding option to add additional flags to rename from maya.cmds

--- src/mb/nodes/node_types.py
+++ src/mb/nodes/node_types.py
@@ -10,13 +10,13 @@ class Node:
     def __init__(self, node_path: str):
         self.node_path = node_path
 
-    def rename(self, new_name: str) -> str:
+    def rename(self, new_name: str, *args, **kwargs) -> str:
         """
         Rename the current node. If new name is not unique maya will resolve a new unique name.
         :param new_name: The new name of the node.
         :return: The new name of the node.
         """
-        actual_new_name = cmds.rename(str(self), new_name)
+        actual_new_name = cmds.rename(str(self), new_name, *args, **kwargs)
         self.node_path = actual_new_name
         return actual_new_name
 
