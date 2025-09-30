640d3be424ee7b5e95891e3349b2f95ce70ad8da
mortenbohne <github@mortenbohne.dk>
2025-10-01 00:04:37 +0200

    refactor: rename mb module to maya_brew and update imports accordingly

--- tests/maya_brew/nodes/test_node_types.py
+++ tests/maya_brew/nodes/test_node_types.py
@@ -1,17 +1,17 @@
 import logging
 
-import mb.nodes.cast
-import mb.nodes.node_types
+import maya_brew.nodes.cast
+import maya_brew.nodes.node_types
 
 
 def test_dag_node_string(test_cube, caplog):
-    dag_node = mb.nodes.node_types.DagNode(test_cube)
-    full_node_path = mb.nodes.cast.get_long_name_from_maya_string(test_cube)
+    dag_node = maya_brew.nodes.node_types.DagNode(test_cube)
+    full_node_path = maya_brew.nodes.cast.get_long_name_from_maya_string(test_cube)
     assert str(dag_node) == full_node_path
     # rename to see warning display correct node path when deleted
     new_name = "new_name"
     dag_node.rename(new_name)
-    mb.cmds.delete(str(dag_node))
+    maya_brew.cmds.delete(str(dag_node))
     with caplog.at_level(logging.WARNING):
         str(dag_node)
         node_path = dag_node.node_path
@@ -19,12 +19,12 @@ def test_dag_node_string(test_cube, caplog):
 
 
 def test_dag_node_rename(test_cube, caplog):
-    dag_node = mb.nodes.node_types.DagNode(test_cube)
+    dag_node = maya_brew.nodes.node_types.DagNode(test_cube)
     new_name = "new_name"
     new_resolved_name = dag_node.rename(new_name)
     assert new_resolved_name == new_name
-    another_cube = mb.cmds.polyCube(name="another_cube")[0]
-    another_dag_node = mb.nodes.node_types.DagNode(another_cube)
+    another_cube = maya_brew.cmds.polyCube(name="another_cube")[0]
+    another_dag_node = maya_brew.nodes.node_types.DagNode(another_cube)
     another_cube_new_name = another_dag_node.rename(new_name)
     assert another_cube_new_name != new_name
     assert another_cube_new_name == "new_name1"
@@ -35,7 +35,7 @@ def test_dag_node_rename(test_cube, caplog):
 
 def test_Transform_repr():
     name = "hello"
-    dag_node = mb.nodes.node_types.Transform.create(name)
+    dag_node = maya_brew.nodes.node_types.Transform.create(name)
     repr = dag_node.__repr__()
     assert name in repr
     assert dag_node.get_full_path() in repr
