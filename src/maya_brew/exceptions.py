2e1a58a8882efc70946896831584ed49bb4fefb0
Morten Bøhne <github@mortenbohne.dk>
2025-10-07 00:14:48 +0200

    19 list attributes on a node (#28)
    Co-authored-by: mortenbohne <13414281+mortenbohne@users.noreply.github.com>

--- src/maya_brew/exceptions.py
+++ src/maya_brew/exceptions.py
@@ -2,3 +2,9 @@ class MayaBrewException(Exception):
     """Base class for all MayaBrew exceptions."""
 
     pass
+
+
+class MayaBrewAttributeError(MayaBrewException, AttributeError):
+    """Raised when there is an attribute-related error in MayaBrew."""
+
+    pass
